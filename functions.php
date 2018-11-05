d6b58d06041d862a0fb34cfbf480be2a9ce6c1d0
Dom Smith <dom.s@visix.co.uk>
2018-11-05 18:01:15 +0000

    Added uses of the base theme filters for asset versioning and localising the script

--- functions.php
+++ functions.php
@@ -9,3 +9,20 @@ add_action('visix_base_loaded', function () {
     visix_init_plugins(['woocommerce', 'forms']);
 
 });
+
+/**
+ * Sets child asset version
+ */
+function blank_child_asset_version() {
+  return '1.0.0';
+}
+add_filter('visix_alter_child_asset_version', 'blank_child_asset_version', 10, 0);
+
+/**
+* Adds admin ajax to script vars
+*/
+function blank_script_vars($script_vars = array()) {
+  $script_vars['ajax_url'] = admin_url('admin-ajax.php');
+  return $script_vars;
+}
+add_filter('visix_alter_script_vars', 'blank_script_vars', 10, 1);
