8632785373216ff60a43c358f4f9c2f7aec922ec
Mat Baxter <mat@mbwebstudios.co.uk>
2018-11-19 11:47:59 +0000

    added some default folders and extra notes within app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,22 @@
+// bootstrap our js application
 require('./bootstrap')
 
+// include the complete Foundation js framework
 import { Foundation } from 'foundation-sites'
 
 // libs from base theme
+// fast click stops the 300ms delay on touch devices when using onClick
 require( 'libs/fastclick' )
+// applies a nice smooth scrolling to all browsers (not required)
 require( 'libs/smoothscroll' )
-
-// dependancies
-// require( 'slick-carousel' )
-// require( 'jquery-zoom/jquery.zoom.min.js' )
-
-// plugins
-// require( 'plugins/woocommerce' )
+// includes Parsely.js validation
+require( 'libs/validation' )
+// includes blazy.js lazy loading
+require( 'libs/lazy-loading' )
 
 $(function () {
+
+    // initialise foundation
     $(document).foundation()
+
 })
