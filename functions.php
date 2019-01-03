f607098c27239fcdb60c952e0b50bbdcbe814156
Micah Kwaka <micahkwaka@ITEM-S54221.localdomain>
2019-01-03 12:36:16 +0000

    post types and acf field for client to add content

--- functions.php
+++ functions.php
@@ -22,5 +22,33 @@ add_filter('visix_alter_child_asset_version', function () {
 */
 add_filter('visix_alter_script_vars', function ($script_vars = array()) {
   $script_vars['ajax_url'] = admin_url('admin-ajax.php');
+  $current_url = home_url($_SERVER['REQUEST_URI']);
+  $script_vars['current_url'] = explode('?',$current_url)[0];
+  $script_vars['current_url_depaged'] = explode('/page', $script_vars['current_url'])[0];
   return $script_vars;
 }, 10, 1);
+
+add_theme_support( 'post-thumbnails' );
+
+if(function_exists('acf_add_options_page')) {
+	acf_add_options_page(array(
+		'page_title' 	=> 'General Information',
+		'menu_title' 	=> 'General Information',
+		'menu_slug' 	=> 'general-information',
+	'icon_url'      => 'dashicons-format-aside',
+	));
+}
+
+function add_file_types_to_uploads($file_types) {
+  $new_filetypes = array();
+  $new_filetypes['svg'] = 'image/svg+xml';
+  $file_types = array_merge($file_types, $new_filetypes );
+  return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
+function remove_menu_items(){
+  remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'remove_menu_items', 999 );
+
+include 'functions/post_types.php';
---
+++ functions/post_types.php
@@ -0,0 +1,178 @@
+<?php
+function create_post_type() {
+  register_post_type('community',
+    array(
+      'labels' => array(
+        'name' => __('Communities'),
+        'singular_name' => __('Community'),
+        'not_found' => __('No Community found.', 'community'),
+        'view_item' => __('View Community', 'community'),
+        'add_new_item' => __( 'Add New Community', 'community' ),
+        'add_new' => __( 'Add New Community', 'community' ),
+        'edit_item' => __( 'Edit Community', 'community' ),
+        'new_item' => __( 'New Community', 'community' ),
+        'menu_name' => _x( 'Communities', 'community' ),
+      ),
+      'public'      => true,
+      'has_archive' => true,
+      'menu_icon'   => 'dashicons-groups',
+      'query_var'   => true,
+      'position'      => 20,
+      'supports'    => array('title'),
+      'rewrite'    => array('slug' => 'community'),
+      'show_in_nav_menus' => true,
+      'hierarchical'  => false,
+    )
+  );
+
+  register_post_type('signatories',
+    array(
+      'labels' => array(
+        'name' => __('Signatories'),
+        'singular_name' => __('Signatory'),
+        'not_found' => __('No Signatory found.', 'signatories'),
+        'view_item' => __('View Signatory', 'signatories'),
+        'add_new_item' => __( 'Add New Signatory', 'signatories' ),
+        'add_new' => __( 'Add New Signatory', 'signatories' ),
+        'edit_item' => __( 'Edit Signatory', 'signatories' ),
+        'new_item' => __( 'New Signatory', 'signatories' ),
+        'menu_name' => _x( 'Signatories', 'signatories' ),
+      ),
+      'public'      => false,
+      'show_ui'     => true,
+      'has_archive' => true,
+      'menu_icon'   => 'dashicons-thumbs-up',
+      'query_var'   => true,
+      'position'    => 20,
+      'supports'    => array('title'),
+      'rewrite'     => array('slug' => 'signatories'),
+      'show_in_nav_menus' => true,
+    )
+  );
+
+  register_post_type('team',
+    array(
+      'labels' => array(
+        'name' => __('Team'),
+        'singular_name' => __('Team'),
+        'not_found' => __('No Team found.', 'team'),
+        'view_item' => __('View Team', 'team'),
+        'add_new_item' => __( 'Add New Team', 'team' ),
+        'add_new' => __( 'Add New Team', 'team' ),
+        'edit_item' => __( 'Edit Team', 'team' ),
+        'new_item' => __( 'New Team', 'team' ),
+        'menu_name' => _x( 'Teams', 'team' ),
+      ),
+      'public'      => false,
+      'show_ui'     => true,
+      'has_archive' => true,
+      'menu_icon'   => 'dashicons-id-alt',
+      'query_var'   => true,
+      'position'    => 30,
+      'supports'    => array('title', 'thumbnail'),
+      'rewrite'     => array('slug' => 'team'),
+      'show_in_nav_menus' => true,
+    )
+  );
+
+  register_post_type('events',
+    array(
+      'labels' => array(
+        'name' => __('Events'),
+        'singular_name' => __('Event'),
+        'not_found' => __('No Event found.', 'events'),
+        'view_item' => __('View Event', 'events'),
+        'add_new_item' => __( 'Add New Event', 'events' ),
+        'add_new' => __( 'Add New Event', 'events' ),
+        'edit_item' => __( 'Edit Event', 'events' ),
+        'new_item' => __( 'New Event', 'events' ),
+        'menu_name' => _x( 'Events', 'events' ),
+      ),
+      'public'      => true,
+      'show_ui'     => true,
+      'has_archive' => true,
+      'menu_icon'   => 'dashicons-megaphone',
+      'query_var'   => true,
+      'position'    => 40,
+      'supports'    => array('title', 'thumbnail', 'editor'),
+      'rewrite'     => array('slug' => 'events'),
+      'show_in_nav_menus' => true,
+    )
+  );
+
+  register_post_type('companies',
+    array(
+      'labels' => array(
+        'name' => __('Companies'),
+        'singular_name' => __('Company'),
+        'not_found' => __('No Company found.', 'companies'),
+        'view_item' => __('View Company', 'companies'),
+        'add_new_item' => __( 'Add New Company', 'companies' ),
+        'add_new' => __( 'Add New Company', 'companies' ),
+        'edit_item' => __( 'Edit Company', 'companies' ),
+        'new_item' => __( 'New Company', 'companies' ),
+        'menu_name' => _x( 'Companies', 'companies' ),
+      ),
+      'public'      => false,
+      'show_ui'     => true,
+      'has_archive' => true,
+      'menu_icon'   => 'dashicons-building',
+      'query_var'   => true,
+      'position'    => 60,
+      'supports'    => array('title', 'thumbnail'),
+      'rewrite'     => array('slug' => 'companies'),
+      'show_in_nav_menus' => true,
+    )
+  );
+
+  register_post_type('resources',
+    array(
+      'labels' => array(
+        'name' => __('Resources'),
+        'singular_name' => __('Resource'),
+        'not_found' => __('No Resource found.', 'resources'),
+        'view_item' => __('View Resource', 'resources'),
+        'add_new_item' => __( 'Add New Resource', 'resources' ),
+        'add_new' => __( 'Add New Resource', 'resources' ),
+        'edit_item' => __( 'Edit Resource', 'resources' ),
+        'new_item' => __( 'New Resource', 'resources' ),
+        'menu_name' => _x( 'Resources', 'resources' ),
+      ),
+      'public'      => true,
+      'show_ui'     => true,
+      'has_archive' => true,
+      'menu_icon'   => 'dashicons-portfolio',
+      'query_var'   => true,
+      'position'    => 42,
+      'supports'    => array('title', 'thumbnail', 'editor'),
+      'rewrite'     => array('slug' => 'resources'),
+      'show_in_nav_menus' => true,
+    )
+  );
+
+  register_post_type('date',
+    array(
+      'labels' => array(
+        'name' => __('Date'),
+        'singular_name' => __('Date'),
+        'not_found' => __('No Date found.', 'date'),
+        'view_item' => __('View Date', 'date'),
+        'add_new_item' => __( 'Add New Date', 'date' ),
+        'add_new' => __( 'Add New Date', 'date' ),
+        'edit_item' => __( 'Edit Date', 'date' ),
+        'new_item' => __( 'New Date', 'date' ),
+        'menu_name' => _x( 'Important Date', 'date' ),
+      ),
+      'public'      => false,
+      'show_ui'     => true,
+      'has_archive' => true,
+      'menu_icon'   => 'dashicons-calendar',
+      'query_var'   => true,
+      'position'    => 30,
+      'supports'    => array('title'),
+      'rewrite'     => array('slug' => 'date'),
+      'show_in_nav_menus' => true,
+    )
+  );
+}
+add_action('init', 'create_post_type', 0);
