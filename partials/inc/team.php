68981e8642e144acf462858700c5e4d7177141e7
Micah Kwaka <micahkwaka@Micahs-MBP.localdomain>
2019-01-29 14:54:29 +0000

    added twitter icon for team

--- partials/inc/team.php
+++ partials/inc/team.php
@@ -12,23 +12,35 @@
           <h3 class="ip-teal h4"><?= get_the_title($info); ?></h3>
 
           <?php $job_title = get_field('job_title', $info); ?>
+          <?php $company = get_field('company', $info); ?>
+
           <?php if ($job_title): ?>
             <p><?= $job_title; ?></p>
           <?php endif; ?>
+
+          <?php if ($company): ?>
+            <span class="ip-pink h4"><?= $company; ?></span>
+          <?php endif; ?>
         </div>
         <div class="cell small-3">
+
           <?php $email = get_field('email', $info); ?>
           <?php $has_twitter = get_field('has_twitter', $info); ?>
           <?php $twitter_username = get_field('twitter_username', $info); ?>
 
-          <?php if ($email): ?>
-            <a href="mailto: <?= get_field('email', $info); ?>" class="button clear orange large"><i class="icon icon-email"></i></a>
+          <div class="grid-x">
+            <?php if ($email): ?>
+              <div class="cell small-6">
+                <a href="mailto: <?= get_field('email', $info); ?>" class="button clear orange large"><i class="icon icon-email"></i></a>
+              </div>
+            <?php endif; ?>
 
-          <?php elseif($has_twitter && !empty($twitter_username)): ?>
-
-            <a href="<?= $twitter_username; ?>" target="_blank" class="button clear orange large"><i class="icon icon-twitter"></i></a>
-
-          <?php endif; ?>
+            <?php if($has_twitter && !empty($twitter_username)): ?>
+              <div class="cell small-6">
+                <a href="<?= $twitter_username; ?>" target="_blank" class="button clear orange large"><i class="icon icon-twitter"></i></a>
+              </div>
+            <?php endif; ?>
+          </div>
         </div>
       </div>
     </div>
