b46d2d8bcb4e5f62f0e280dc2e7b8a148f6ad5e4
Micah Kwaka <micahkwaka@Micahs-MBP.localdomain>
2019-02-05 15:07:13 +0000

    fix empty text for select box

--- partials/plugins/forms/inputs/select.php
+++ partials/plugins/forms/inputs/select.php
@@ -5,8 +5,9 @@
 
         <?php if (is_page('sign-up-to-the-ip-inclusive-charter')): ?>
           <option value=""><?= 'Please Select'; ?></option>  
-        <?php endif; ?>
+        <?php else: ?>
         <option value=""><?= (isset($field['placeholder']) ? $field['placeholder'] : 'Please Select'); ?></option>
+		<?php endif; ?>
       <?php endif; ?>
       <?php foreach ($field['choices'] as $value => $label) : ?>
         <option value="<?= $value; ?>" <?php selected($value, $field['value']); ?> <?= (isset($field['option']) && isset($field['option']['attributes']) ? acf_esc_atts($field['option']['attributes']) : ''); ?>><?= $label; ?></option>
