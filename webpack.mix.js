0bcd3c943a34cd3ea68546e6028454d9c0bfa34f
Micah Kwaka <micahkwaka@Micahs-MBP.localdomain>
2019-02-19 10:26:17 +0000

    reduce font size of nav buttons on ie

--- webpack.mix.js
+++ webpack.mix.js
@@ -35,7 +35,6 @@ mix.sass('resources/scss/app.scss', 'assets/css/app.css', {
 mix.browserSync({
   proxy: 'ipinclusive.work',
   notify: false,
-  https: true,
   files: [
     "resources/**/*",
     "*.php",
